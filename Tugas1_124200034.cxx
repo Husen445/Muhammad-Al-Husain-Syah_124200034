3c7123311a59a8b93dd13035cb7e4b51a872072c
HUSEN <husen4845@gmail.com>
2020-10-17 13:31:28 +0700

    Tugas Algoritma

--- Tugas1_124200034.cxx
+++ Tugas1_124200034.cxx
@@ -1,25 +1,18 @@
-//Program Menghitung Nilai UTS dan UAS
+//Program Menghitung Rata-Rata Nilai UTS dan UAS
 #include <iostream>
 using namespace std;
 main ()
 {
-	int NIM , NilaiUTSMKA1, NilaiUTSMKA2, NilaiUASMKA1, NilaiUASMKA2, RataRataNilaiUTS, RataRataNilaiUAS, JumlahMKAUTS, JumlahMKAUAS;
+	int NIM , NilaiUTS, NilaiUAS, RataRataNilai;
 	
 	cout<< "PROGRAM MENGHITUNG NILAI UTS dan UAS\n";
 	cout<< "Masukkan Nama  = Muhammad Al Husain Syah\n";
 	cout<< "Masukkan NIM   = "; cin>> NIM;
 	cout<< "--------------------------------------------------\n";
-	cout<< "NilaiUTSMKA1  = "; cin>> NilaiUTSMKA1;
-	cout<< "NilaiUTSMKA2  = "; cin>> NilaiUTSMKA2;
-	cout<< "JumlahMKAUTS  = "; cin>> JumlahMKAUTS;
-	cout<< "NilaiUASMKA1  = "; cin>> NilaiUASMKA1;
-	cout<< "NilaiUASMKA2  = "; cin>> NilaiUASMKA2;
-	cout<< "JumlahMKAUAS  = "; cin>> JumlahMKAUAS;
-	RataRataNilaiUTS = NilaiUTSMKA1 + NilaiUTSMKA2 / JumlahMKAUTS;
-	RataRataNilaiUAS = NilaiUASMKA1 + NilaiUASMKA2 / JumlahMKAUAS;   
+	cout<< "NilaiUTS  = "; cin>> NilaiUTS;
+	cout<< "NilaiUAS  = "; cin>> NilaiUAS;
+	RataRataNilai = (NilaiUTS + NilaiUAS)/ 2;  
 	cout<< "\nHASIL:\n";
-	cout<< "RataRataNilaiUTS = " << RataRataNilaiUTS << endl;
-	cout<< "RataRataNilaiUAS = " << RataRataNilaiUAS << endl;
+	cout<< "RataRataNilai = " << RataRataNilai << endl;
 	cout<< "---------------------------------------------------";
 }
-
